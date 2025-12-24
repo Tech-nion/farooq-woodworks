fdb8a12f5f137f22f77d14db6c53dd90ff008aba
Zuniara-Farooq <geekstech29@gmail.com>
2025-12-24 23:05:01 +0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,1 +1,9 @@
+// src/lib/supabase.ts
+import { createClient } from '@supabase/supabase-js'
+
+// Use your .env variables
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
 
